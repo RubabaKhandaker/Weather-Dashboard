265539bb49b98b008c29e03dff303d0955f0dc95
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2024-01-08 17:28:27 +1100

    Added function to fetch weather data from api to script.js + header to index.html

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
 // plugin of dayjs for date format
 dayjs.extend(window.dayjs_plugin_localizedFormat);
 
+const apiKey = '';
+
+// fetching weather data from api
+function getWeatherCurrent(City) {
+    let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${apiKey}&units=metric`; 
+    fetch(API_URL) 
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Error (${response.statusText}). Invalid city. Please enter a valid city and try again.`);
+      }
+    })
+    .then(function (forecastData){
+        showWeatherCurrent(forecastData);
+        searchSave(forecastDataData);
+    })
+    .catch(function (error) {
+      alert('Unable to fetch current weather data: ' + error.message);
+    });
+}
